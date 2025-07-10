7899bf80b0e591d756caefefc72b68ae08fe7198
Timey <157982461+timey07@users.noreply.github.com>
2025-07-10 10:19:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -28,6 +28,6 @@ def extract_text():
 import os
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 80))
+    port = int(os.environ.get("PORT", 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
 
